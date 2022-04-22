33380253577d8a7fe3a72b0f656ae37052a86d1c
Sebastian <rolo0828@gmail.com>
2022-04-22 17:16:15 -0500

    Una propuesta

--- js/grid.js
+++ js/grid.js
@@ -50,4 +50,27 @@ window.addEventListener('load', () => {
   overlay.addEventListener('click', (evento) => {
     evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
   });
+
+
+const overlay = document.getElementById('overlay');
+  document.querySelectorAll('.nuestros-programas .carta').forEach((elemento) => {
+    elemento.addEventListener('click', () => {
+      const ruta = elemento.getAttribute('src');
+      const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+      overlay.classList.add('activo');
+      document.querySelector('#overlay').src = ruta;
+      document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+    });
+  });
+
+  // Eventlistener del boton de cerrar
+  document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+    overlay.classList.remove('activo');
+  });
+
+  // Eventlistener del overlay
+  overlay.addEventListener('click', (evento) => {
+    evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+  });
 });
